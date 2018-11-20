f04e77e72671715de153376aa5c5aa8b9ed19f42
kbar1 <44809624+kbar1@users.noreply.github.com>
2018-11-20 15:05:30 +0100

    Update check.py

--- check.py
+++ check.py
@@ -1,5 +1,6 @@
 import numpy as np
 from solution import *
+from obpng import *
 
 # Dostateczna
 print("- Ocena dostateczna")
@@ -10,7 +11,7 @@ image = square(512, 128, (32, 64))
 write_png(image, 'results/1_square.png')
 
 # Kółko
-"""
+
 print("  kółka")
 image = midcircle((512, 256))
 write_png(image, 'results/2_circle_1.png')
@@ -18,14 +19,12 @@ image = midcircle((256, 512))
 write_png(image, 'results/2_circle_2.png')
 image = midcircle((512, 512))
 write_png(image, 'results/2_circle_3.png')
-"""
+
 
 # Szachownica
-"""
 print("  szachownica")
 image = checkerboard(256)
 write_png(image, 'results/3_checkerboard.png')
-"""
 
 # Dobra
 lenna = np.squeeze(read_png('data/mono/lenna.png'))
@@ -37,8 +36,8 @@ print("  interpolacja najbliższych sąsiadów")
 image = nn_interpolation(lenna, (100, 100))
 image = nn_interpolation(image, (512, 512))
 write_png(image, 'results/4_nn.png')
-"""
 
+"""
 ## Interpolacja dwuliniowa
 """
 print("- Ocena bardzo dobra")
